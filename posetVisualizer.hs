f7a2a4e53afe9db059291e9c67418c4d00e5ea70
Ammar Husain <ammar.s.husain@gmail.com>
2018-04-20 17:57:29 -0700

    poset Visualization
    Combines the Diagrams package and Math.Combinat.Poset to draw Hasse diagrams. There is a problem with layers so it only draws the arrows for adjacent layers which are ordered by total number of predecessors rather than minimal number of predecessors.

---
+++ posetVisualizer.hs
@@ -0,0 +1,95 @@
+import Diagrams.Backend.SVG.CmdLine
+
+{-# LANGUAGE NoMonomorphismRestriction #-}
+import Diagrams.Prelude
+import Data.List
+import Data.Ord (comparing)
+import Data.Function (on)
+import Data.Maybe (fromMaybe)
+import Data.Colour.SRGB (sRGB24read)
+
+import qualified Math.Combinatorics.Poset as PS
+
+data Subset = Subset Int [Int]
+
+addDigit :: Int -> Int -> Int
+addDigit num d = 10*num+d
+
+name :: Subset -> Int
+name (Subset x elts) = foldl addDigit 0 elts
+name2 :: [Int] -> Int
+name2 elts = foldl addDigit 0 elts
+
+(Subset _ elts1) `isSubset` (Subset _ elts2) = all (`elem` elts2) elts1
+
+subsetsBySize :: Int -> [[Subset]]
+subsetsBySize n = map (map (Subset n))
+                . groupBy ((==) `on` length)
+                . sortBy (comparing length)
+                . subsequences
+                $ [1..n]
+
+level :: PS.Poset t -> t -> t -> Int
+level ps bottom current = length $ PS.interval ps (bottom,current)
+
+breakPosetByLevel :: PS.Poset t -> t -> [[t]]
+breakPosetByLevel (PS.Poset (set,po)) bottom = groupBy (\x y -> (myLevel x == myLevel y)) $ sortBy (\x y -> (compare (myLevel x) (myLevel y))) set
+                                              where myLevel = (\z -> (level (PS.Poset (set,po)) bottom z))
+
+node :: Int -> Diagram B
+node n = text (show n) # fontSizeL 0.2 # fc white <> square 1 # fc blue
+
+drawElts n elts = hcat
+                . map (\i -> if i `elem` elts
+                               then node i
+                               else strutX 1
+                      )
+                $ [1..n]
+
+drawSet (Subset n elts) = (drawElts n elts # centerXY
+                            <> rect (fromIntegral n + 0.5) 1.5
+                                 # dashingG [0.2,0.2] 0
+                                 # lw thin
+                                 # named (name2 elts)
+                          )
+drawItem n = node n # named n
+
+hasseRow = centerX . hcat' (with & sep .~ 2) . map drawSet
+
+hasseDiagram n = setsD # centerXY
+  where setsD = vcat' (with & sep .~ fromIntegral n)
+              . map hasseRow
+              . reverse
+              $ subsets
+        subsets = subsetsBySize n
+
+hasseDiagram2 ps bottom = setsD # centerXY
+    where setsD = vcat' (with & sep .~ fromIntegral 1)
+           . map (centerX . hcat' (with & sep .~ 2) . map drawItem)
+           $ breakPosetByLevel ps bottom
+
+toConnect :: Int -> [(Int,Int)]
+toConnect n = concat $ zipWith connectSome (subsetsBySize n) (tail $ subsetsBySize n)
+connectSome :: [Subset] -> [Subset] -> [(Int,Int)]
+connectSome subs1 subs2 = [ (name s1, name s2) | s1 <- subs1
+                                             , s2 <- subs2
+                                             , s1 `isSubset` s2 ]
+
+withConnections n = (hasseDiagram n) # applyAll [connectOutside' (with & gaps       .~ small
+                          & headLength .~ local 0.15) j k | (j,k) <- toConnect n]
+
+toConnect2:: PS.Poset t -> t -> [(t,t)]
+toConnect2 ps bottom = concat $ zipWith (connectSome2 ps) (broken) (tail $ broken) where broken=(breakPosetByLevel ps bottom)
+connectSome2 :: PS.Poset t -> [t] -> [t] -> [(t,t)]
+connectSome2 (PS.Poset (set,po)) layeri layerj = [ (s1, s2) | s1 <- layeri
+                                             , s2 <- layerj
+                                             , s1 `po` s2 ]
+withConnections2 ps bottom = (hasseDiagram2 ps bottom) # applyAll [connectOutside' (with & gaps       .~ small
+                          & headLength .~ local 0.15) j k | (j,k) <- toConnect2 ps bottom]
+
+--example = pad 1.1 $ hasseDiagram2 (PS.posetD 24) 1
+example = pad 1.1 $ withConnections2 (PS.posetD 24) 1
+--example = pad 1.1 $ withConnections 4
+--myExample n = pad 1.1 $ withConnections n
+
+main = mainWith (example :: Diagram B)
