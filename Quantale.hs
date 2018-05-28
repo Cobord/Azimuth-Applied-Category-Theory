eb4dd07e80f371ee3743ca630f8ce240ed86fcd1
Ammar Husain <ammar.s.husain@gmail.com>
2018-05-27 17:13:16 -0700

    Quantales
    TypeClass for quantale, matrix multiplication to go from V-weighted graph to V-category

---
+++ Quantale.hs
@@ -0,0 +1,54 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE DataKinds, TypeSynonymInstances, TypeFamilies, TypeOperators #-}
+{-# LANGUAGE UndecidableInstances, FlexibleInstances #-}
+
+module Quantale where
+
+import qualified Data.Set as Set
+--import qualified Data.Number.Fin.Integer as Fin
+import Data.Maybe
+
+class UnitalQuantale a where
+    unit :: a
+    po :: a -> a -> Bool
+    monoidal :: a -> a -> a
+    myJoin :: [a] -> a
+
+greatestLowerBound :: Cost -> Cost -> Cost
+greatestLowerBound Nothing x = x
+greatestLowerBound x Nothing = x
+greatestLowerBound (Just x) (Just y)
+                                    | x<=y = Just x
+                                    | otherwise = Just y
+
+type Cost = Maybe Int
+instance UnitalQuantale Cost where
+    po Nothing Nothing = True
+    po Nothing (Just x) = True
+    po (Just x) Nothing = False
+    po (Just x) (Just y) = (x >= y)
+    monoidal _ Nothing = Nothing
+    monoidal Nothing _ = Nothing
+    monoidal (Just x) (Just y) = Just (x+y)
+    unit = Just 0
+    myJoin [] = Nothing
+    myJoin (x:xs) = greatestLowerBound x (myJoin xs)
+
+data MyVertices = VertexX | VertexY | VertexZ
+allVertices = [VertexX , VertexY , VertexZ]
+
+weightGraph :: MyVertices -> MyVertices -> Cost
+weightGraph VertexX VertexX = unit
+weightGraph VertexX VertexY = Just 4
+weightGraph VertexX VertexZ = Just 3
+weightGraph VertexY VertexY = unit
+weightGraph VertexY VertexX = Just 3
+weightGraph VertexZ VertexZ = unit
+weightGraph VertexZ VertexY = Just 4
+weightGraph _ _ = Nothing
+
+multiplyQMatrices :: (UnitalQuantale q) => [b] -> (a -> b -> q) -> (b -> c -> q) -> a -> c -> q
+multiplyQMatrices allY matrixM matrixN x z = myJoin [monoidal (matrixM x y) (matrixN y z) | y <- allY]
+
+example = multiplyQMatrices allVertices weightGraph weightGraph VertexZ VertexX
